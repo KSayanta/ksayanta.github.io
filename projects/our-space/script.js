e1c2c45a575cfc9e3d3e59b702c087d89c7d49f5
KSayanta <sayantakarmakar1999@gmail.com>
2025-04-12 01:20:02 +0530

    add func to add new post

--- projects/our-space/script.js
+++ projects/our-space/script.js
@@ -1,12 +1,95 @@
 const url = "https://apis.scrimba.com/jsonplaceholder/posts";
+let postsArr = [];
 
+// DOM Elements
+const btnNewPost = document.getElementById("btn-new-post");
+const postTemplate = document.getElementById("post-template").content;
+
+// Custom elements
+customElements.define(
+  "modal-post",
+  class extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+    connectedCallback() {
+      const template = document.getElementById("form-template").content;
+      this.appendChild(template.cloneNode(true));
+
+      // Elements
+      this.newPostForm = this.querySelector("#modal-form__post");
+      this.btnExit = this.querySelector("#post-btn-exit");
+      this.btnPost = this.querySelector("#post-btn");
+      this.blogsWrapper = document.querySelector("blogs-post .blogs");
+
+      this.btnExit.addEventListener("click", e => {
+        e.preventDefault();
+        this.closeModal();
+      });
+
+      this.btnPost.addEventListener("click", e => {
+        e.preventDefault();
+        const title = this.querySelector("#post-title").value;
+        const body = this.querySelector("#post-body").value;
+        const opt = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ title: title, body: body }),
+        };
+
+        fetch(url, opt)
+          .then(res => res.json())
+          .then(data => {
+            postsArr.unshift(data);
+            this.blogsWrapper.prepend(newBlogPost(data));
+            this.blogsWrapper.removeChild(this.blogsWrapper.lastElementChild);
+            this.closeModal();
+          });
+      });
+    }
+
+    // Handler functions
+    closeModal() {
+      document.body.removeChild(this);
+    }
+  }
+);
+
+// Event listners
+btnNewPost.addEventListener("click", displayModal);
+
+/**
+ * Fetch posts from api
+ */
 fetch(url)
   .then(res => res.json())
   .then(data => {
-    handlePages(data);
+    postsArr = data;
+    displayPosts();
   });
 
-function handlePages(postsArr) {
+/**
+ * Handler functions
+ */
+function newBlogPost(post) {
+  const newBlogPost = postTemplate.cloneNode(true);
+  const postHeading = newBlogPost.querySelector(".post-heading");
+  const postBody = newBlogPost.querySelector(".post-body");
+
+  postHeading.setAttribute("data-id", post.id);
+  postHeading.textContent = post.title;
+  postBody.textContent = post.body;
+
+  return newBlogPost;
+}
+
+function displayModal() {
+  const modal = document.createElement("modal-post");
+  document.body.appendChild(modal);
+}
+
+function displayPosts() {
   customElements.define(
     "blogs-post",
     class extends HTMLElement {
@@ -15,32 +98,32 @@ function handlePages(postsArr) {
       constructor() {
         // Always runs first
         super();
+      }
 
+      connectedCallback() {
         // Attach copy of template to light DOM
         const template = document.getElementById("blog-template").content;
         this.appendChild(template.cloneNode(true));
-
-        // Variables
-        this.currPage = 1;
 
         // Elements
         this.pageNo = this.querySelector("#pageNo");
         this.btnPrev = this.querySelector("#btnPrev");
         this.btnNext = this.querySelector("#btnNext");
         this.blogsWrapper = this.querySelector(".blogs");
-        this.postTemplate = document.getElementById("post-template").content;
 
         // Events
         this.btnPrev.addEventListener("click", this);
         this.btnNext.addEventListener("click", this);
-      }
-
-      attributeChangedCallback(_Name, _OldVal, newVal) {
-        this.entriesPerPage = newVal;
-        this.totalPages = Math.ceil(postsArr.length / this.entriesPerPage);
 
         // Init
         this.updatePage();
+      }
+
+      attributeChangedCallback(_Name, _OldVal, newVal) {
+        // Variables
+        this.currPage = 1;
+        this.entriesPerPage = newVal;
+        this.totalPages = Math.ceil(postsArr.length / this.entriesPerPage);
       }
 
       // Event handlers
@@ -62,15 +145,7 @@ function handlePages(postsArr) {
           this.blogsWrapper.removeChild(this.blogsWrapper.firstElementChild);
 
         postsArr.slice(this.fromPage, this.toPage).forEach(post => {
-          const newBlogPost = this.postTemplate.cloneNode(true);
-          const postHeading = newBlogPost.querySelector(".post-heading");
-          const postBody = newBlogPost.querySelector(".post-body");
-
-          postHeading.setAttribute("data-id", post.id);
-          postHeading.textContent = post.title;
-          postBody.textContent = post.body;
-
-          this.blogsWrapper.appendChild(newBlogPost);
+          this.blogsWrapper.appendChild(newBlogPost(post));
         });
 
         this.updateBtn();
