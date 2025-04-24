87b799db591d884219354216f982c7dcaff7b8bf
KSayanta <sayantakarmakar1999@gmail.com>
2025-04-24 19:09:11 +0530

    feat: add score system

--- projects/game-of-war/script.js
+++ projects/game-of-war/script.js
@@ -5,6 +5,8 @@ const btnDraw = document.getElementById("draw");
 const imgWrapperNL = document.querySelectorAll(".image-wrapper");
 const resultText = document.getElementById("feedbackText");
 const counterStat = document.getElementById("counterRem");
+const scoreBot = document.getElementById("scoreBot");
+const scorePlayer = document.getElementById("scorePlayer");
 
 const cardsArr = [
   "2",
@@ -22,43 +24,63 @@ const cardsArr = [
   "ACE",
 ];
 let deckId;
+let score = [0, 0];
 
 window.addEventListener("DOMContentLoaded", () => {
-  update();
+  initGame();
 });
 btnNew.addEventListener("click", newDeck);
 btnDraw.addEventListener("click", drawCards);
 
-function update(data = {}) {
-  btnDraw.removeAttribute("disabled");
+function initGame() {
   !deckId && btnDraw.setAttribute("disabled", "");
-  !data.remaining && btnDraw.setAttribute("disabled", "");
 }
 
-function newDeck() {
-  fetch(url + "new/shuffle/")
-    .then(res => res.json())
-    .then(data => {
-      deckId = data.deck_id;
-      counterStat.innerText = `Remaining Cards: ${data.remaining}`;
-      update(data);
-    });
+function endGame() {
+  btnDraw.setAttribute("disabled", "");
+
+  if (score[0] < score[1]) resultText.innerText = "You Won the Game!";
+  else if (score[0] > score[1]) resultText.innerText = "I Win the Game!";
+  else resultText.innerText = "It's a Draw!";
 }
 
-function drawCards() {
-  clear();
-  fetch(url + deckId + "/draw/?count=2")
-    .then(res => res.json())
+async function newDeck() {
+  // init new deck from api
 
-    .then(data => {
-      counterStat.innerText = `Remaining Cards: ${data.remaining}`;
-      update(data);
-      display(data.cards);
-    });
+  const data = await fetch(url + "new/shuffle/").then(res => res.json());
+
+  deckId = data.deck_id;
+  counterStat.innerText = `Remaining Cards: ${data.remaining}`;
+
+  // enable draw button
+  btnDraw.removeAttribute("disabled");
+}
+
+async function drawCards() {
+  // fetches cards from api
+
+  const data = await fetch(url + deckId + "/draw/?count=2").then(res =>
+    res.json()
+  );
+
+  counterStat.innerText = `Remaining Cards: ${data.remaining}`;
+  display(data.cards);
+
+  setTimeout(() => {
+    // delay before updating score
+    updateScore(data.cards);
+
+    // game over
+    if (!data.remaining) endGame();
+  }, 1000);
 }
 
 function display(cardsArr) {
-  // @param: array of cards
+  // renders cards in viewport
+  // @param   {array} cards
+
+  clear(); // clear prev cards first
+
   cardsArr.forEach((card, idx) => {
     const imgElm = document.createElement("img");
     imgElm.src = card.image;
@@ -66,25 +88,36 @@ function display(cardsArr) {
 
     imgElm.onload = () => {
       imgWrapperNL[idx].appendChild(imgElm);
-      updateScore(cardsArr);
     };
   });
 }
 
 function updateScore(cardsArr) {
-  resultText.innerHTML = compareCards(cardsArr[0], cardsArr[1]);
+  // updates player score
+  // @param   {array} card objects
+
+  const result = compareCards(cardsArr[0], cardsArr[1]);
+  resultText.innerHTML = result.text;
+  Number.isInteger(result.playerIdx) && (score[result.playerIdx] += 1);
+  scoreBot.innerText = score[0];
+  scorePlayer.innerText = score[1];
 }
 
 function compareCards(card1, card2) {
-  // @param two cards object
+  // compares cards to determine winner
+  // @param   {object} first card
+  // @param   {object} second card
+  // @return  {object} text and player id, player id is null when draw
 
   const card1Score = cardsArr.indexOf(card1.value);
   const card2Score = cardsArr.indexOf(card2.value);
-  if (card1Score > card2Score) return "I Win!";
-  if (card1Score < card2Score) return "You Won!";
-  if (card1Score === card2Score) return "War!";
+  if (card1Score > card2Score) return { playerIdx: 0, text: "I Win!" };
+  if (card1Score < card2Score) return { playerIdx: 1, text: "You Won!" };
+  if (card1Score === card2Score) return { playerIdx: null, text: "War!" };
 }
 
 function clear() {
+  // clears card images and feedback text
+  resultText.innerHTML = "...";
   imgWrapperNL.forEach(elm => (elm.innerHTML = ""));
 }
