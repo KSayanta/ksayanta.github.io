6bd7e98ed5d21b32e00d3aa9c51c02f1224fdd10
KSayanta <sayantakarmakar1999@gmail.com>
2025-05-13 08:26:09 +0530

    feat: add dice faces

--- projects/spa/tenzies/components/Die.jsx
+++ projects/spa/tenzies/components/Die.jsx
@@ -4,11 +4,10 @@ export default function Die({ value, isHeld, onClick }) {
   return (
     <button
       className="btn die"
+      data-value={value}
       aria-label={`Die with value ${value}, ${value ? "held" : "not held"}`}
       aria-pressed={isHeld}
       onClick={onClick}
-    >
-      {value}
-    </button>
+    ></button>
   );
 }
